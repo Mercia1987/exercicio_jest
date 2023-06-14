cd914bd5fb8db740586535797a884104b9cd2d1a
Mercia1987 <126789281+Mercia1987@users.noreply.github.com>
2023-06-14 08:54:06 -0300

    primeira parte exercicio jest

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+const calculadora = {
+    soma(numero_1, numero_2) {
+        return numero_1 + numero_2;
+    },
+    subtracao(numero_1, numero_2) {
+
+    }
+};
+
+module.exports = {
+    calculadora
+};
---
+++ calculadora.test.js
@@ -0,0 +1,9 @@
+const { calculadora } = require('./calculadora');
+
+test('soma de 2 numeros', () => {
+    expect(calculadora.soma(2, 2)).toBe(4);
+});
+
+test('subtracao de 2 numeros', () => {
+
+})
